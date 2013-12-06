63fd10e614ab28ec08236dc1935c4ccbb8896bfa
Andy Venet <andy.venet@colaboracionvirtual.com>
2013-12-06 12:15:20 -0300

    Improving coding style of utils.py

--- datatables_demo/demo/utils.py
+++ datatables_demo/demo/utils.py
@@ -2,21 +2,28 @@ from django.db.models import Q
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.utils.cache import add_never_cache_headers
-from django.utils import simplejson
+import json
+
 
 def get_datatables_records(request, querySet, columnIndexNameMap, jsonTemplatePath = None, *args):
     """
     Usage:
         querySet: query set to draw data from.
         columnIndexNameMap: field names in order to be displayed.
-        jsonTemplatePath: optional template file to generate custom json from.  If not provided it will generate the data directly from the model.
-
+        jsonTemplatePath: optional template file to generate custom json from.
+        If not provided it will generate the data directly from the model.
     """
+    #Gets the number of columns
+    cols = int(request.GET.get('iColumns', 0))
 
-    cols = int(request.GET.get('iColumns',0)) # Get the number of columns
-    iDisplayLength =  min(int(request.GET.get('iDisplayLength',10)),100)	 #Safety measure. If someone messes with iDisplayLength manually, we clip it to the max value of 100.
-    startRecord = int(request.GET.get('iDisplayStart',0)) # Where the data starts from (page)
-    endRecord = startRecord + iDisplayLength  # where the data ends (end of page)
+    #Safety measure.If someone messes with iDisplayLength manually, we clip it to the max value of 100.
+    iDisplayLength = min(int(request.GET.get('iDisplayLength', 10)), 100)
+
+    #Where the data starts from (page)
+    startRecord = int(request.GET.get('iDisplayStart', 0))
+
+    #Where the data ends (end of page)
+    endRecord = startRecord + iDisplayLength
 
     # Pass sColumns
     keys = columnIndexNameMap.keys()
@@ -25,13 +32,14 @@ def get_datatables_records(request, querySet, columnIndexNameMap, jsonTemplatePa
     sColumns = ",".join(map(str,colitems))
 
     # Ordering data
-    iSortingCols =  int(request.GET.get('iSortingCols',0))
+    iSortingCols = int(request.GET.get('iSortingCols', 0))
     asortingCols = []
 
     if iSortingCols:
         for sortedColIndex in range(0, iSortingCols):
-            sortedColID = int(request.GET.get('iSortCol_'+str(sortedColIndex),0))
-            if request.GET.get('bSortable_{0}'.format(sortedColID), 'false')  == 'true':  # make sure the column is sortable first
+            sortedColID = int(request.GET.get('iSortCol_'+str(sortedColIndex), 0))
+            #Makes sure the column is sortable first
+            if request.GET.get('bSortable_{0}'.format(sortedColID), 'false') == 'true':
                 sortedColName = columnIndexNameMap[sortedColID]
                 sortingDirection = request.GET.get('sSortDir_'+str(sortedColIndex), 'asc')
                 if sortingDirection == 'desc':
@@ -39,13 +47,14 @@ def get_datatables_records(request, querySet, columnIndexNameMap, jsonTemplatePa
                 asortingCols.append(sortedColName)
         querySet = querySet.order_by(*asortingCols)
 
-    # Determine which columns are searchable
+    #Determines which columns are searchable
     searchableColumns = []
-    for col in range(0,cols):
-        if request.GET.get('bSearchable_{0}'.format(col), False) == 'true': searchableColumns.append(columnIndexNameMap[col])
+    for col in range(0, cols):
+        if request.GET.get('bSearchable_{0}'.format(col), False) == 'true':
+            searchableColumns.append(columnIndexNameMap[col])
 
-    # Apply filtering by value sent by user
-    customSearch = request.GET.get('sSearch', '').encode('utf-8');
+    #Applies filtering by value sent by user
+    customSearch = request.GET.get('sSearch', '').encode('utf-8')
     if customSearch != '':
         outputQ = None
         first = True
@@ -57,34 +66,50 @@ def get_datatables_records(request, querySet, columnIndexNameMap, jsonTemplatePa
     # Individual column search
     outputQ = None
     for col in range(0,cols):
-        if request.GET.get('sSearch_{0}'.format(col), False) > '' and request.GET.get('bSearchable_{0}'.format(col), False) == 'true':
+        if request.GET.get('sSearch_{0}'.format(col), False) > '' and \
+                        request.GET.get('bSearchable_{0}'.format(col), False) == 'true':
             kwargz = {columnIndexNameMap[col]+"__icontains" : request.GET['sSearch_{0}'.format(col)]}
             outputQ = outputQ & Q(**kwargz) if outputQ else Q(**kwargz)
-    if outputQ: querySet = querySet.filter(outputQ)
 
-    iTotalRecords = iTotalDisplayRecords = querySet.count() #count how many records match the final criteria
-    querySet = querySet[startRecord:endRecord] #get the slice
-    sEcho = int(request.GET.get('sEcho',0)) # required echo response
+    if outputQ:
+        querySet = querySet.filter(outputQ)
+
+    #Counts how many records match the final criteria
+    iTotalRecords = iTotalDisplayRecords = querySet.count()
+
+    #Gets the slice
+    querySet = querySet[startRecord:endRecord]
+
+    #Required echo response
+    sEcho = int(request.GET.get('sEcho', 0))
 
     if jsonTemplatePath:
-        jstonString = render_to_string(jsonTemplatePath, locals()) #prepare the JSON with the response, consider using : from django.template.defaultfilters import escapejs
-        response = HttpResponse(jstonString, mimetype="application/javascript")
+        #Prepares the JSON with the response, consider using : from django.template.defaultfilters import escapejs
+        jsonString = render_to_string(jsonTemplatePath, locals())
+
+        response = HttpResponse(jsonString, mimetype="application/javascript")
     else:
         aaData = []
-        a = querySet.values()
-        for row in a:
-            rowkeys = row.keys()
-            rowvalues = row.values()
+        queryset_values = querySet.values()
+        for queryset_row in queryset_values:
+            rowkeys = queryset_row.keys()
+            rowvalues = queryset_row.values()
             rowlist = []
-            for col in range(0,len(colitems)):
+            for col in range(0, len(colitems)):
                 for idx, val in enumerate(rowkeys):
                     if val == colitems[col]:
-                        rowlist.append(str(rowvalues[idx]))
+                        rowlist.append(unicode(rowvalues[idx]))
             aaData.append(rowlist)
-        response_dict = {}
-        response_dict.update({'aaData':aaData})
-        response_dict.update({'sEcho': sEcho, 'iTotalRecords': iTotalRecords, 'iTotalDisplayRecords':iTotalDisplayRecords, 'sColumns':sColumns})
-        response =  HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+
+        response_dict = {
+            'aaData': aaData,
+            'sEcho': sEcho,
+            'iTotalRecords': iTotalRecords,
+            'iTotalDisplayRecords': iTotalDisplayRecords,
+            'sColumns': sColumns
+        }
+
+        response = HttpResponse(json.dumps(response_dict), mimetype='application/javascript')
     #prevent from caching datatables result
     add_never_cache_headers(response)
     return response
